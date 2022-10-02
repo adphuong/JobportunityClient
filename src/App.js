ea95f80a6fdcf663de568b07f0de674326d72281
adphuong <aprilphuong@gmail.com>
2022-10-02 14:47:23 -0700

    Jobs display correctly in Table

--- src/App.js
+++ src/App.js
@@ -23,32 +23,38 @@ function App() {
       <h1> Home Page </h1>
       { jobs ? (
         <>
-          {jobs.map(job => {
-            return(
-                <Table striped bordered hover>
-                    <thead>
-                    <tr>
-                        <th>Company</th>
-                        <th>Position</th>
-                        <th>Stage</th>
-                        <th>Next Step</th>
-                        <th>Date Applied</th>
-                        <th>Notes</th>
+            <Table striped bordered hover>
+                <thead>
+                <tr>
+                    <th>Company</th>
+                    <th>Position</th>
+                    <th>Stage</th>
+                    <th>Next Step</th>
+                    <th>Date Applied</th>
+                    <th>Notes</th>
+                </tr>
+                </thead>
+                <tbody>
+                    {jobs.map((job, index) => (
+                    <tr data-index={index}>
+                        <td>{job.company}</td>
+                        <td>{job.position}</td>
+                        <td>{job.stage}</td>
+                        <td>{job.next_step}</td>
+                        <td>{job.date_applied}</td>
+                        <td>{job.notes}</td>
                     </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>{job.company}</td>
-                            <td>{job.position}</td>
-                            <td>{job.stage}</td>
-                            <td>{job.next_step}</td>
-                            <td>{job.date_applied}</td>
-                            <td>{job.Notes}</td>
-                        </tr>
-                    </tbody>
-              </Table>
-            );
-          })}
+                    ))}
+                    {/* <tr>
+                        <td>{job.company}</td>
+                        <td>{job.position}</td>
+                        <td>{job.stage}</td>
+                        <td>{job.next_step}</td>
+                        <td>{job.date_applied}</td>
+                        <td>{job.Notes}</td>
+                    </tr> */}
+                </tbody>
+            </Table>
         </>
       ) : ""}
       <Button onClick = {() => navigate("add-job")}>NEXT</Button>
