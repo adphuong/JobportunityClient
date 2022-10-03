6ba30088e2bfbb1e5a548ec1c2d107849f47eb56
adphuong <aprilphuong@gmail.com>
2022-10-03 14:07:43 -0700

    Add floating label to add-job form

--- src/AddJob.js
+++ src/AddJob.js
@@ -1,4 +1,4 @@
-import { Button, Form } from "react-bootstrap";
+import { Button, FloatingLabel, Form } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Stack from 'react-bootstrap/Stack';
@@ -50,76 +50,112 @@ function AddJob() {
 			<h1 className="title">Add a Job</h1>
 			<Form>
 				<Form.Group>
-					<Form.Control 
-						name="company"
-						value={job.company} 
-						placeholder="Company" 
-						onChange={handleChange}
-						required
-					/>
-					<Form.Control 
-						name="position" 
-						value={job.position} 
-						placeholder="Position" 
-						onChange={handleChange}
-						required
-					/>
-					<Form.Select
-						name="stage" 
-						value={job.stage}  
-						onChange={handleChange}
-						required
-					>	
-						<option>Select stage</option>
-						<option value="Prospect">Prospect</option>
-						<option value="Applied">Applied</option>
-						<option value="Phone Screen">Phone Screen</option>
-						<option value="Online Assessment">Online Assessment</option>
-						<option value="Interview: Phone">Interview: Phone</option>
-						<option value="Interview: Video">Interview: Virtual</option>
-						<option value="Interview: In-office">Interview: In-office</option>
-						<option value="Negotiating Offer">Negotiating Offer</option>
-						<option value="Rejection">Rejection</option>
-						<option value="Closed">Closed</option>
-						<option value="Offer">Offer</option>
-					</Form.Select>
-					<Form.Select 
-						name="next_step" 
-						value={job.next_step} 
-						placeholder="Next Step" 
-						onChange={handleChange}
-						required
+					<FloatingLabel
+						controlId="floatingInput"
+						label="Company"
+						className="mb-3"
 					>
-						<option>Select next step</option>
-						<option value="Apply">Apply</option>
-						<option value="Research">Research</option>
-						<option value="Follow-up Application">Follow-up Application</option>
-						<option value="Do interview(s)">Do interview(s)</option>
-						<option value="Email: Thank you">Email: Thank you</option>
-						
-					</Form.Select>
-					<Form.Control 
-						type="date"
-						name="date_found" 
-						value={job.date_found} 
-						placeholder="Date Found" 
-						onChange={handleChange}
-						required pattern="\m{2}-\d{2}-\d{4}"
-					/>
-					<Form.Control 
-						type="date"
-						name="date_applied" 
-						value={job.date_applied} 
-						placeholder="Date Applied" 
-						onChange={handleChange}
-						required pattern="\m{2}-\d{2}-\d{4}"
-					/>
-					<Form.Control 
-						name="notes" 
-						value={job.notes} 
-						placeholder="Notes" 
-						onChange={handleChange}
-					/>
+						<Form.Control 
+							name="company"
+							value={job.company} 
+							placeholder="Company" 
+							onChange={handleChange}
+							required
+						/>
+					</FloatingLabel>
+					<FloatingLabel
+						controlId="floatingInput"
+						label="Position"
+						className="mb-3"
+					>
+						<Form.Control 
+							name="position" 
+							value={job.position} 
+							placeholder="Position" 
+							onChange={handleChange}
+							required
+						/>
+					</FloatingLabel>
+					<FloatingLabel
+						controlId="floatingInput"
+						label="Stage"
+						className="mb-3"
+					>
+						<Form.Select
+							name="stage" 
+							value={job.stage}  
+							onChange={handleChange}
+							required
+						>	
+							<option value="Prospect">Prospect</option>
+							<option value="Applied">Applied</option>
+							<option value="Phone Screen">Phone Screen</option>
+							<option value="Online Assessment">Online Assessment</option>
+							<option value="Interview: Phone">Interview: Phone</option>
+							<option value="Interview: Video">Interview: Virtual</option>
+							<option value="Interview: In-office">Interview: In-office</option>
+							<option value="Negotiating Offer">Negotiating Offer</option>
+							<option value="Rejection">Rejection</option>
+							<option value="Closed">Closed</option>
+							<option value="Offer">Offer</option>
+						</Form.Select>
+					</FloatingLabel>
+					<FloatingLabel
+						controlId="floatingInput"
+						label="Next Step"
+						className="mb-3"
+					>
+						<Form.Select 
+							name="next_step" 
+							value={job.next_step} 
+							onChange={handleChange}
+							required
+						>
+							<option value="Apply">Apply</option>
+							<option value="Research">Research</option>
+							<option value="Follow-up Application">Follow-up Application</option>
+							<option value="Do interview(s)">Do interview(s)</option>
+							<option value="Email: Thank you">Email: Thank you</option>
+							
+						</Form.Select>
+					</FloatingLabel>
+					<FloatingLabel
+						controlId="floatingInput"
+						label="Date Found"
+						className="mb-3"
+					>
+						<Form.Control 
+							type="date"
+							name="date_found" 
+							value={job.date_found} 
+							onChange={handleChange}
+							required pattern="\m{2}-\d{2}-\d{4}"
+						/>
+					</FloatingLabel>
+					<FloatingLabel
+						controlId="floatingInput"
+						label="Date Applied"
+						className="mb-3"
+					>
+						<Form.Control 
+							type="date"
+							name="date_applied" 
+							value={job.date_applied} 
+							onChange={handleChange}
+							required pattern="\m{2}-\d{2}-\d{4}"
+							/>
+					</FloatingLabel>
+					<FloatingLabel
+						controlId="floatingInput"
+						label="Notes"
+						className="mb-3"
+					>
+						<Form.Control 
+							name="notes" 
+							value={job.notes} 
+							onChange={handleChange}
+						/>
+					</FloatingLabel>
 				</Form.Group>
 				
 				
