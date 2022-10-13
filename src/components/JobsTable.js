86efdc5bc60dd62ed1ec0478fd9c4d3899dda1df
adphuong <aprilphuong@gmail.com>
2022-10-13 12:17:08 -0700

    Fixed minor bug with stage value for Interview: Virtual

--- src/components/JobsTable.js
+++ src/components/JobsTable.js
@@ -302,7 +302,7 @@ function JobsTable() {
 						<option value="Phone Screen">Phone Screen</option>
 						<option value="Online Assessment">Online Assessment</option>
 						<option value="Interview: Phone">Interview: Phone</option>
-						<option value="Interview: Video">Interview: Virtual</option>
+						<option value="Interview: Virtual">Interview: Virtual</option>
 						<option value="Interview: In-office">Interview: In-office</option>
 						<option value="Negotiating Offer">Negotiating Offer</option>
 						<option value="Rejection">Rejection</option>
